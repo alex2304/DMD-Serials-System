71ab446e679029481c4568fdff14d8e8a36fdf5f
Fanis <fmkalimullin@gmail.com>
2016-10-29 18:02:32 +0300

    [+] index.html and bootstrap

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,14 @@
+from flask import render_template
+
 from . import main
 
 
 @main.route('/')
-def hello_world():
-    return 'This is a main page content'
+def index():
+    return render_template('index.html')
+
+
+@main.route('/serial/<int:serial_id>')
+def serial(serial_id):
+    serial_info = serial_id  # TODO: need to retrieve serial information form DB
+    return render_template('serial.html', serial_info=serial_info)

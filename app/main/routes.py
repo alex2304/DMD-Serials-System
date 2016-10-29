37154532b8ae8429d58edfdd34677306b172fa9e
Fanis <fmkalimullin@gmail.com>
2016-10-29 21:33:23 +0300

    [+] gener cloud and some new pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,6 @@
+from flask import redirect
 from flask import render_template
+from flask import url_for
 
 from . import main
 
@@ -16,5 +18,24 @@ def serials(serial_id=None):
         return render_template('serial_info.html', serial_info=serial_info)
 
     return render_template('serials.html')
+
+
+@main.route('episodes/<int:episode_id>')
+def episodes(episode_id=None):
+    episode_info = episode_id  # TODO: need to retrieve serial information form DB
+    if episode_id is not None:
+        return render_template('episode_info.html', episode_info=episode_info)
+
+    return redirect(url_for('serials'))
+
+
+@main.route('actors/')
+@main.route('actors/<int:actor_id>')
+def actors(actor_id=None):
+    actor_info = actor_id  # TODO: need to retrieve actor information form DB
+    if actor_id is not None:
+        return render_template('actor_info.html', actor_info=actor_info)
+
+    return render_template('actors.html')
 
 

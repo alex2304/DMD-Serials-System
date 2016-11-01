3dcc637fdc282e5a55e86c2793f1da2366985c6a
Aleksey <alex2304el@gmail.com>
2016-11-02 01:52:31 +0300

    Mapping adapted for new SQL tables

--- sql/create_empty_tables.sql
+++ sql/create_empty_tables.sql
@@ -1,152 +1,222 @@
-CREATE TABLE Serials (
+CREATE TABLE serial (
   serial_id SERIAL PRIMARY KEY,
   title CHAR(100) NOT NULL,
-  release_year DATE NOT NULL,
+  release_date DATE NOT NULL,
   country CHAR(50) NOT NULL
 );
 
-CREATE TABLE Genres (
+CREATE TABLE genre (
 	genre_title CHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE SerialsGengres (
-  serial_id INTEGER REFERENCES Serials (serial_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  genre_title CHAR(50) REFERENCES Genres (genre_title) ON DELETE NO ACTION ON UPDATE CASCADE
-);
-
-CREATE TABLE Users(
-  login CHAR(20) PRIMARY KEY,
-  vk_id INTEGER UNIQUE NOT NULL,
-  email CHAR(50) UNIQUE NOT NULL
-);
-
-CREATE TABLE Subscribes (
-  user_login CHAR(20),
+CREATE TABLE serial_has_genre (
   serial_id INTEGER,
-  PRIMARY KEY (user_login, serial_id),
-  FOREIGN KEY (serial_id) REFERENCES Serials(serial_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (user_login) REFERENCES Users (login) ON DELETE CASCADE ON UPDATE CASCADE
+  genre_title CHAR(50),
+
+  FOREIGN KEY (serial_id) REFERENCES serial (serial_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (genre_title) REFERENCES genre (genre_title) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
-CREATE TABLE Reviews (
-  user_login CHAR(20) REFERENCES Users (login) ON DELETE CASCADE ON UPDATE CASCADE,
-  serial_id INTEGER REFERENCES Serials(serial_id) ON DELETE CASCADE ON UPDATE CASCADE,
+CREATE TABLE app_user(
+  login CHAR(20) PRIMARY KEY,
+  vk_id INTEGER NOT NULL,
+  email CHAR(50) NOT NULL,
+
+  CONSTRAINT vk_id_must_be_unique UNIQUE (vk_id),
+  CONSTRAINT email_must_be_unique UNIQUE (email)
+);
+
+CREATE TABLE subscribes (
+  app_user_login CHAR(20),
+  serial_id INTEGER,
+
+  PRIMARY KEY (app_user_login, serial_id),
+  FOREIGN KEY (serial_id) REFERENCES serial (serial_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (app_user_login) REFERENCES app_user (login) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE reviews (
+  app_user_login CHAR(20),
+  serial_id INTEGER,
   title CHAR(50),
   text CHAR(10000) NOT NULL,
   review_date DATE NOT NULL ,
-  PRIMARY KEY (user_login, serial_id)
+
+  PRIMARY KEY (app_user_login, serial_id),
+  FOREIGN KEY (app_user_login) REFERENCES app_user (login) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (serial_id) REFERENCES serial (serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Seasons (
-  season_id SERIAL PRIMARY KEY,
+CREATE TABLE season (
   season_number INTEGER NOT NULL,
-  serial_id INTEGER NOT NULL REFERENCES Serials(serial_id) ON DELETE CASCADE ON UPDATE CASCADE
+  serial_id INTEGER NOT NULL,
+
+  PRIMARY KEY (season_number, serial_id),
+  FOREIGN KEY (serial_id) REFERENCES serial(serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Comments (
+CREATE TABLE comments (
   comment_id SERIAL PRIMARY KEY,
   comment_date DATE NOT NULL,
   text CHAR(1000) NOT NULL,
+  app_user_login CHAR(20),
+  season_number INTEGER,
+  serial_id INTEGER,
 
-  user_login CHAR(20) REFERENCES Users (login) ON DELETE CASCADE ON UPDATE CASCADE,
-  season_id INTEGER REFERENCES Seasons(season_id) ON DELETE CASCADE ON UPDATE CASCADE
+  FOREIGN KEY (app_user_login) REFERENCES app_user (login) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (season_number, serial_id) REFERENCES season (season_number, serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Episodes (
-  episode_id SERIAL PRIMARY KEY,
-  episode_number INTEGER NOT NULL,
+CREATE TABLE episode (
   title CHAR(20) NOT NULL,
   release_date DATE NOT NULL,
   duration INTEGER NOT NULL,
-  rating INTEGER NOT NULL CONSTRAINT positive_rating CHECK (rating > 0 AND rating < 11),
+  rating INTEGER NOT NULL,
+  episode_number INTEGER NOT NULL,
+  season_number INTEGER,
+  serial_id INTEGER,
 
-  season_id INTEGER REFERENCES Seasons (season_id) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (episode_number, season_number, serial_id),
+  FOREIGN KEY (season_number, serial_id) REFERENCES season (season_number, serial_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT positive_rating CHECK (rating > 0 AND rating < 11)
 );
 
-CREATE TABLE Person (
+CREATE TABLE person (
   person_id SERIAL PRIMARY KEY,
   name CHAR(50) NOT NULL,
   birthdade DATE NOT NULL,
   genger CHAR(1) NOT NULL
 );
 
-CREATE TABLE Director (
-  director_id INTEGER REFERENCES Person (person_id) PRIMARY KEY
+CREATE TABLE director (
+  director_id INTEGER REFERENCES person (person_id) PRIMARY KEY
 );
 
-CREATE TABLE Writer (
-  writer_id INTEGER REFERENCES Person (person_id) PRIMARY KEY
+CREATE TABLE writer (
+  writer_id INTEGER REFERENCES person (person_id) PRIMARY KEY
 );
 
-CREATE TABLE Creator (
-  creator_id INTEGER REFERENCES Person (person_id) PRIMARY KEY
+CREATE TABLE creator (
+  creator_id INTEGER REFERENCES person (person_id) PRIMARY KEY
 );
 
-CREATE TABLE Actor (
-  actor_id INTEGER REFERENCES Person (person_id) PRIMARY KEY
+CREATE TABLE actor (
+  actor_id INTEGER REFERENCES person (person_id) PRIMARY KEY
 );
 
-CREATE TABLE Role (
+CREATE TABLE role (
   role_id SERIAL PRIMARY KEY,
   title CHAR(50) NOT NULL
 );
 
-CREATE TABLE Played (
+CREATE TABLE plays (
   played_id SERIAL PRIMARY KEY,
-  role_id INTEGER REFERENCES Role (role_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  actor_id INTEGER REFERENCES Actor (actor_id) ON DELETE CASCADE ON UPDATE CASCADE,
-  UNIQUE (role_id, actor_id)
+  role_id INTEGER,
+  actor_id INTEGER,
+
+  CONSTRAINT pair_role_id_actor_id_must_be_unique UNIQUE (role_id, actor_id),
+  FOREIGN KEY (role_id) REFERENCES role (role_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (actor_id) REFERENCES actor (actor_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Filmed (
-  played_id INTEGER REFERENCES Played (played_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  episode_id INTEGER REFERENCES Episodes (episode_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  PRIMARY KEY (played_id, episode_id)
+CREATE TABLE films (
+  played_id INTEGER,
+  episode_number INTEGER,
+  season_number INTEGER,
+  serial_id INTEGER,
+  PRIMARY KEY (played_id, episode_number, season_number, serial_id),
+
+  FOREIGN KEY (played_id) REFERENCES plays (played_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (episode_number, season_number, serial_id) REFERENCES
+    episode (episode_number, season_number, serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Created (
-  creator_id INTEGER REFERENCES Creator (creator_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  serial_id INTEGER REFERENCES Serials (serial_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  PRIMARY KEY (creator_id, serial_id)
+CREATE TABLE creates (
+  creator_id INTEGER,
+  serial_id INTEGER,
+
+  PRIMARY KEY (creator_id, serial_id),
+  FOREIGN KEY (creator_id) REFERENCES creator (creator_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (serial_id) REFERENCES serial (serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Directs (
-  director_id INTEGER REFERENCES Director (director_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  episode_id INTEGER REFERENCES Episodes (episode_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  PRIMARY KEY (director_id, episode_id)
+CREATE TABLE directs (
+  director_id INTEGER,
+  episode_number INTEGER,
+  season_number INTEGER,
+  serial_id INTEGER,
+
+  PRIMARY KEY (director_id, episode_number, season_number, serial_id),
+  FOREIGN KEY (director_id) REFERENCES director (director_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (episode_number, season_number, serial_id) REFERENCES
+    episode (episode_number, season_number, serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Writes (
-  writer_id INTEGER REFERENCES Writer (writer_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  episode_id INTEGER REFERENCES Episodes (episode_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  PRIMARY KEY (writer_id, episode_id)
+CREATE TABLE writes (
+  writer_id INTEGER,
+  episode_number INTEGER,
+  season_number INTEGER,
+  serial_id INTEGER,
+
+  PRIMARY KEY (writer_id, episode_number, season_number, serial_id),
+  FOREIGN KEY (writer_id) REFERENCES writer (writer_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (episode_number, season_number, serial_id) REFERENCES
+    episode (episode_number, season_number, serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Actor_award (
+CREATE TABLE actor_award (
   award_title CHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE Serials_award (
+CREATE TABLE serial_award (
   award_title CHAR(50) PRIMARY KEY
 );
 
-CREATE TABLE Role_awarded (
+CREATE TABLE role_has_award (
   year DATE,
-  award_title CHAR(50) REFERENCES Actor_award (award_title) ON DELETE CASCADE ON UPDATE CASCADE ,
-  played_id INTEGER REFERENCES Played (played_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  episode_id INTEGER REFERENCES Episodes (episode_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  PRIMARY KEY (played_id, episode_id, award_title)
+  award_title CHAR(50),
+  played_id INTEGER,
+  episode_number INTEGER,
+  season_number INTEGER,
+  serial_id INTEGER,
+
+  PRIMARY KEY (played_id, episode_number, season_number, serial_id, award_title),
+  FOREIGN KEY (award_title) REFERENCES actor_award (award_title) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (played_id) REFERENCES plays (played_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (episode_number, season_number, serial_id) REFERENCES
+    episode (episode_number, season_number, serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE Serials_awarded (
+CREATE TABLE serial_has_award (
   year DATE,
-  award_title CHAR(50) REFERENCES Serials_award (award_title) ON DELETE CASCADE ON UPDATE CASCADE ,
-  creator_id INTEGER REFERENCES Creator (creator_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  serial_id INTEGER REFERENCES Serials (serial_id) ON DELETE CASCADE ON UPDATE CASCADE ,
-  PRIMARY KEY (creator_id, serial_id, award_title)
+  award_title CHAR(50),
+  creator_id INTEGER,
+  serial_id INTEGER,
+
+  PRIMARY KEY (creator_id, serial_id, award_title),
+  FOREIGN KEY (award_title) REFERENCES serial_award (award_title) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (creator_id) REFERENCES creator (creator_id) ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (serial_id) REFERENCES serial (serial_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO Serials(title, release_year, country) VALUES ('Test Serial', '2015-10-11', 'Russia');
-INSERT INTO Seasons(season_number, serial_id) VALUES (1, 1);
-INSERT INTO Episodes(episode_number, title, release_date, duration, rating, season_id) VALUES(1, 'xyu Ha ga4e', '2015-07-16', 60, 5, 1);
-INSERT INTO Episodes(episode_number, title, release_date, duration, rating, season_id) VALUES(1, 'TPELLI B rapa}|{E', '2015-07-17', 50, 4, 1);
+CREATE OR REPLACE FUNCTION check_data_release() RETURNS TRIGGER AS $check_data_release$
+  BEGIN
+    IF (NEW.release_date < (SELECT s.release_date
+                           FROM serial s
+                           WHERE (NEW.serial_id = s.serial_id)))
+    THEN
+    RAISE EXCEPTION 'serial release_date must be smaller than episode release_date';
+    END IF;
+    RETURN NEW;
+  END;
+$check_data_release$ LANGUAGE plpgsql;
+
+--DROP TRIGGER check_episode_data_release on episode;
+CREATE TRIGGER check_episode_data_release
+  BEFORE INSERT OR UPDATE ON episode
+  FOR EACH ROW EXECUTE PROCEDURE check_data_release();
+
+INSERT INTO Serial(title, release_date, country) VALUES ('Test Serial', '2015-10-11', 'Russia');
+INSERT INTO Season(season_number, serial_id) VALUES (1, 1);
+INSERT INTO Episode(episode_number, title, release_date, duration, rating, season_number, serial_id) VALUES(1, 'xyu Ha ga4e', '2015-11-16', 60, 5, 1, 1);
+INSERT INTO Episode(episode_number, title, release_date, duration, rating, season_number, serial_id) VALUES(2, 'TPELLI B rapa}|{E', '2015-11-17', 50, 4, 1, 1);

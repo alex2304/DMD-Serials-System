a4c3c193aa463a8b5894920a9f69508dfc345408
Aleksey <alex2304el@gmail.com>
2016-11-19 20:14:34 +0300

    Restored sql functions

--- run.py
+++ run.py
@@ -13,9 +13,15 @@ def drop_create_db(_app):
 
     connection = connect_db()
     with _app.open_resource('../sql/drop_tables.sql', mode='r') as f:
+        # try:
         connection.cursor().execute(f.read())
+        # except:
+        #     pass
     with _app.open_resource('../sql/create_tables_with_data.sql', mode='r') as f:
+        # try:
         connection.cursor().execute(f.read())
+        # except:
+        #     pass
     connection.commit()
     print(' * Database has been successfully recreated')
     connection.close()

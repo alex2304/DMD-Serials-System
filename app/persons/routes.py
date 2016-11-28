f23ad3efe995acfd0d5eb3ad88326496939f774c
Fanis <fmkalimullin@gmail.com>
2016-11-28 13:31:36 +0300

    [+] pages for actors, writers, directors

--- app/persons/routes.py
+++ app/persons/routes.py
@@ -3,7 +3,29 @@ from flask import render_template
 from . import persons
 
 
-@persons.route('actors/')
-def process_actors():
+@persons.route('/actors/<int:actor_id>')
+@persons.route('/actors')
+def process_actors(actor_id = None):
 
-    return render_template('serials.html')
+    if actor_id:
+        return render_template('actor_info.html')
+
+    return render_template('actors.html')
+
+
+
+@persons.route('/writers/<int:writer_id>')
+@persons.route('/writers')
+def process_writers(writer_id = None):
+    if writer_id:
+        return render_template('writer_info.html')
+
+    return render_template('writers.html')
+
+@persons.route('/directors/<int:director_id>')
+@persons.route('/directors')
+def process_directors(director_id = None):
+    if director_id:
+        return render_template('director_info.html')
+
+    return render_template('directors.html')
